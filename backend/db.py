f6d2e5ab8da97c1208581034384939b1c3986a5a
Alexandr Parhomenko <alexandr21@yandex-team.ru>
2018-07-15 02:36:01 +0300

    Add db

---
+++ backend/db.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+class DBConnection(object):
+    def __init__(self, host='localhost', dbname='testdb', user='alexandr21'):
+        self.__host = host
+        self.__dbname = dbname
+        self.__user = user
+
+    def get_conn_string(self):
+        conn_string = "host='{}' dbname='{}' user='{}'".format(self.__host,
+                        self.__dbname, self.__user)
+        return conn_string
+
+    def __enter__(self):
+        self.__dbconn = psycopg2.connect(self.get_conn_string()) 
+        return self.__dbconn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # make sure the dbconnection gets closed
+        self.__dbconn.commit()
+        self.__dbconn.close()
--- backend/handlers.py
+++ backend/handlers.py
@@ -1,18 +1,17 @@
 from tornado import gen, httpclient, ioloop, queues, web
 from tornado.httpclient import AsyncHTTPClient
 import tornado
-import asyncio
-import mimetypes
 import urllib
 import logging
 import os
 import json
-from uuid import uuid4
 import random
 import requests
 import string
 from utils import add_url_params, gen_string
-from functools import partial
+from db import DBConnection
+import psycopg2
+import psycopg2.extras
 
 logger = logging.getLogger(__name__)
 
@@ -32,7 +31,30 @@ class SendMessage(web.RequestHandler):
         data.update({"access_token": self.__token,
                      "email": self.__email})
 
+    def avia_handler(self, data, files):
+        result = random.randint(0, 1)
+        extra_info = {'departure_time': '15.07.2018 01:25:41',
+                      'arrive_time': '21.07.2018 02:13:33',
+                      'from': 'Moscow',
+                      'to': 'Kiev'}
+        return False, extra_info
+
+    def passport_handler(self, data, files):
+        result = random.randint(0, 1)
+        extra_info = {'name': 'Ivan',
+                      'surname': 'Ivanov',
+                      'sex': 'm'}
+        return True, extra_info
+
+    def apply_handlers(self, Id, data, files):
+        is_avia, extra_avia = self.avia_handler(data, files)
+        is_passport, extra_passport = self.passport_handler(data, files)
+        with DBConnection() as conn:
+            cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            cur.execute("""INSERT INTO messages VALUES (%s,%s,%s,%s,%s)""", (Id, is_passport, is_avia, json.dumps(extra_passport), json.dumps(extra_avia)))
+
     def upload_files(self, Id, data):
+        file_names = []
         attaches_list = []
         for k, (info,) in self.request.files.items():
             name, content_type = info['filename'], info['content_type']
@@ -41,7 +63,7 @@ class SendMessage(web.RequestHandler):
                 output_file = open(fname, 'wb')
                 output_file.write(info['body'])
                 output_file.close()
-
+                file_names.append(fname)
                 logger.debug("file " + name + " is uploaded") 
 
                 client = AsyncHTTPClient()
@@ -57,19 +79,21 @@ class SendMessage(web.RequestHandler):
                 attaches_list.append({'id': attach_id, 'type': 'attach'})
                 logger.debug("file upload result: {}".format(str(res.text)))
         data.update({'attaches': {'list' : attaches_list}})
+        return file_names
 
     async def post(self):
         data = tornado.escape.json_decode(self.request.files['body'][0]['body'])
         self.append_params(data)
         Id = gen_string(32)
 
-        self.upload_files(Id, data)
+        filenames = self.upload_files(Id, data)
         
         data.update({"id": Id})
         logger.debug("Send message id: {}".format(data.get('id'))) 
         client = AsyncHTTPClient()
         url = add_url_params(self.__url, data)
         req = tornado.httpclient.HTTPRequest(url, body='', method='POST')
+        self.apply_handlers(Id, data, filenames)
         res = await client.fetch(req)
         self.write(str(res.body))
         self.finish()
