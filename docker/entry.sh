8baf1369a1e2ecef3a1c2da11ad838c59bb2cf86
Alexandr Parhomenko <alexandr21@yandex-team.ru>
2018-07-15 16:19:55 +0300

    Add entry point, up Dockerfile

---
+++ docker/entry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+su postgres -c "/usr/lib/postgresql/9.5/bin/pg_ctl  -D /usr/local/pgsql/data  start"
+su postgres -c "createdb call_of_data"
+su postgres -c "psql 'host=localhost port=5432 dbname=call_of_data user=postgres' -f /sql/init.sql"
