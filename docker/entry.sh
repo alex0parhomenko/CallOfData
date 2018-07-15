a8dcdd745a4fa586e3344f929b93fbdd6e1d8327
Alexandr Parhomenko <alexandr21@yandex-team.ru>
2018-07-15 17:10:38 +0300

    fix

--- docker/entry.sh
+++ docker/entry.sh
@@ -4,4 +4,4 @@ su postgres -c "/usr/lib/postgresql/9.5/bin/pg_ctl  -D /usr/local/pgsql/data  st
 su postgres -c "createdb call_of_data"
 su postgres -c "psql 'host=localhost port=5432 dbname=call_of_data user=postgres' -f /sql/init.sql"
 
-python3.5 /backend/main.py --port 8080 --dbname=call_of_data --pg_user=postgres &
+python3.6 /backend/main.py --port 8080 --dbname=call_of_data --pg_user=postgres &
